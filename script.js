50ef88a634ca451584e8b959633334b735f0f8c9
BVIDev <16726593+bviengineer@users.noreply.github.com>
2017-11-30 03:47:22 -0500

    refactored function to globally replace all apperances of a dash with an underscore

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Function accepts a single kebab-cased string argument and returns the snake_cased version
 
 function kebabToSnake(input){
-    var snakeCase = input.replace("-", "_");
+    var snakeCase = input.replace(/-/g, "_");
     return snakeCase;
 }
